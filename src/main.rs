40f30b2f018aa1876f87bbbffee044251de71ba6
5witchkr <hob4419@gmail.com>
2024-04-14 21:58:02 +0900

    skeleton code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod dml;
 mod ddl;
 mod types;
+mod access_manager;
+mod buffer_manager;
+mod disk_manager;
+mod query_executor;
+mod query_planner;
+mod syntax_analysis;
 
 fn main() {
     println!("Hello, world!");
